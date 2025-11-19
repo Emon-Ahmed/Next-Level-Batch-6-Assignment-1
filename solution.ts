d81036fa3823faa8cec86b7fd2a746cf81b1e1cf
Emon-Ahmed <technicalboyemon@gmail.com>
2025-11-19 18:45:56 +0600

    docs: problem 1

--- solution.ts
+++ solution.ts
@@ -1,6 +1,13 @@
-/*
-- This solution.ts file is an example; replace it with your own code.
-- Use the same function names and parameter names as in the problem statement.
-- Write only the code inside the function bodies and return the result.
-- Do not use any console.log statements or comments.
-*/
+const formatValue = (
+  value: string | number | boolean
+): string | number | boolean => {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 10;
+  } else if (typeof value === "boolean") {
+    return !value;
+  } else {
+    return value;
+  }
+};
