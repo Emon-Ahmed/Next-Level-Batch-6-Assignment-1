56eeb54e55aca007788851b1308ecaf4bc21425e
Emon-Ahmed <technicalboyemon@gmail.com>
2025-11-19 19:04:20 +0600

    docs: problem 2 & 3

--- solution.ts
+++ solution.ts
@@ -11,3 +11,27 @@ const formatValue = (
     return value;
   }
 };
+
+const getLength = (value: string | any[]): number => {
+  if (typeof value === "string") {
+    return value.length;
+  } else if (Array.isArray(value)) {
+    return value.length;
+  } else {
+    return 0;
+  }
+};
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}`;
+  }
+}
